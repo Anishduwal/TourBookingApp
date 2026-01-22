2f30fc2a20d8a87d25e918360875821712fbfcf5
Anish <anish.dwl1.ad@gmail.com>
2026-01-22 22:34:35 +0545

    commit changes and merged conflict fix

--- TourBooking.API/obj/Debug/net8.0/TourBooking.API.AssemblyInfo.cs
+++ TourBooking.API/obj/Debug/net8.0/TourBooking.API.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourBooking.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86b7f80948d8fb2370deabd648457ab8a5b810da")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce8539917aa7a5feb6f88d345d2f6dbaf04ea76c")]
->>>>>>> b6328909a48428338848fb23b836f72e47775c77
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92af0f3c716240338bcfd8b9c53257ac54941368")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourBooking.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourBooking.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TourBooking.Domain/obj/Debug/net8.0/TourBooking.Domain.AssemblyInfo.cs
+++ TourBooking.Domain/obj/Debug/net8.0/TourBooking.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourBooking.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc80d72c6396aed47b83106cb9f3db8b536b221e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92af0f3c716240338bcfd8b9c53257ac54941368")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourBooking.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourBooking.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
