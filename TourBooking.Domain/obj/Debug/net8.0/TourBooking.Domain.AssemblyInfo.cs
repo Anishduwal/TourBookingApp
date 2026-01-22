57737678509195b938826d2129c1868cbb7ee3cf
Anish <anish.dwl1.ad@gmail.com>
2026-01-22 22:09:35 +0545

    changes

--- TourBooking.Domain/obj/Debug/net8.0/TourBooking.Domain.AssemblyInfo.cs
+++ TourBooking.Domain/obj/Debug/net8.0/TourBooking.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourBooking.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6328909a48428338848fb23b836f72e47775c77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b51d90381563d5223e08fc941bb245648ae4652c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourBooking.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourBooking.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
